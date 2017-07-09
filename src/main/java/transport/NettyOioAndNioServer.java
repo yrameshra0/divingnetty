a6728edba07b10ee1c8e08084b094556e57384a4
Y Ramesh Rao <rameshrao.yeduvakula@in.tesco.com>
2017-07-09 19:29:48 +0100

    Netty oio nio transport switch difference illustration

---
+++ src/main/java/transport/NettyOioAndNioServer.java
@@ -0,0 +1,68 @@
+package transport;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.oio.OioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.channel.socket.oio.OioServerSocketChannel;
+
+import java.net.InetSocketAddress;
+
+import static io.netty.buffer.Unpooled.copiedBuffer;
+import static io.netty.buffer.Unpooled.unreleasableBuffer;
+import static io.netty.util.CharsetUtil.UTF_8;
+
+public class NettyOioAndNioServer {
+    public static void main(String[] args) throws InterruptedException {
+        NettyOioAndNioServer nettyOioAndNioServer = new NettyOioAndNioServer();
+        nettyOioAndNioServer.serverWithNio(1901);
+    }
+
+    public void serverWithOio(int port) throws InterruptedException {
+        server(port, new OioEventLoopGroup(), OioServerSocketChannel.class);
+    }
+
+    public void serverWithNio(int port) throws InterruptedException {
+        server(port, new NioEventLoopGroup(), NioServerSocketChannel.class);
+    }
+
+    public void server(int port, EventLoopGroup eventLoopGroup, Class<? extends ServerChannel> serverChannelClazz) throws InterruptedException {
+        final ByteBuf messageBuffer = unreleasableBuffer(copiedBuffer("Hi \r\n", UTF_8));
+
+        try {
+            ServerBootstrap serverBootstrap = new ServerBootstrap();
+            serverBootstrap
+                    .group(eventLoopGroup)
+                    .channel(serverChannelClazz)
+                    .localAddress(new InetSocketAddress(port))
+                    .childHandler(inboundChannelHandler(messageBuffer));
+
+            ChannelFuture future = serverBootstrap.bind().sync();
+            future.channel().closeFuture().sync();
+        } finally {
+            eventLoopGroup.shutdownGracefully();
+        }
+    }
+
+    private ChannelHandler inboundChannelHandler(ByteBuf message) {
+        return new ChannelInitializer<SocketChannel>() {
+            @Override
+            protected void initChannel(SocketChannel socketChannel) throws Exception {
+                socketChannel.pipeline().addLast(inboundChannelHandlerAdapter(message));
+            }
+        };
+    }
+
+    private ChannelHandler inboundChannelHandlerAdapter(ByteBuf message) {
+        return new ChannelInboundHandlerAdapter() {
+
+            @Override
+            public void channelActive(ChannelHandlerContext ctx) throws Exception {
+                ctx.writeAndFlush(message).addListener(ChannelFutureListener.CLOSE);
+            }
+        };
+    }
+}
